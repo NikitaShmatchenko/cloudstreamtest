1d4afa2c57edd54c0598dba22457ed55c3767e95
Nikita Shmatchenko <nikita.shmatchenko@cengage.com>
2020-10-08 02:47:28 +0300

    Changed producing strategy from uuid generation to a rest endpoint where a string can be posted to trigger a producing event.

--- cloudstreamtest-producer/src/main/java/com/nshm/cloudstreamtest/producer/Producer.java
+++ cloudstreamtest-producer/src/main/java/com/nshm/cloudstreamtest/producer/Producer.java
@@ -1,22 +1,30 @@
 package com.nshm.cloudstreamtest.producer;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.UUID;
-import java.util.function.Supplier;
+import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Producer {
+
+    private static final String BINDING_NAME = "produce-out-0";
+
+    @Autowired
+    private StreamBridge streamBridge;
 
     public static void main(String[] args) {
         SpringApplication.run(Producer.class, args);
     }
 
-    @Bean
-    public Supplier<String> generate() {
-        return () -> UUID.randomUUID().toString();
+    @PostMapping("/string/{value}")
+    public String submit(@PathVariable String value) {
+        streamBridge.send(BINDING_NAME, value);
+        return "String" + value + "has been published.";
     }
 }
--- cloudstreamtest-producer/src/test/java/com/nshm/cloudstreamtest/producer/ProducerTest.java
+++ cloudstreamtest-producer/src/test/java/com/nshm/cloudstreamtest/producer/ProducerTest.java
@@ -2,6 +2,7 @@ package com.nshm.cloudstreamtest.producer;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.stream.binder.test.OutputDestination;
@@ -11,28 +12,36 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 
 import java.nio.charset.StandardCharsets;
+import java.util.UUID;
 
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+//TODO Investigate why message doesn't show up after submit is called.
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class ProducerTest {
 
-    private static final int UUID_LENGTH = 36;
+    @Autowired
+    private Producer producer;
 
     @Test
     public void productUUID() {
+        String generatedValue = UUID.randomUUID().toString();
+
         try (ConfigurableApplicationContext context = new SpringApplicationBuilder(
                 TestChannelBinderConfiguration.getCompleteConfiguration(
                         Producer.class))
-                .run("--spring.cloud.function.definition=generate")) {
+                .run("--spring.cloud.function.definition=produce")) {
+
+            producer.submit(generatedValue);
+
             OutputDestination target = context.getBean(OutputDestination.class);
 
             byte[] message = target.receive().getPayload();
             assertNotNull(message);
             String stringMessage = new String(message, StandardCharsets.UTF_8);
-            assertEquals(stringMessage.length(), UUID_LENGTH);
+            assertEquals(stringMessage, generatedValue);
         }
     }
 }
